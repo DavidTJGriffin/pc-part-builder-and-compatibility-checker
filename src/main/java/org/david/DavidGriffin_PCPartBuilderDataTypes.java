c41d574b6989aeb65a6d38a39c0ce8a2d8f67272
DavidTJGriffin <david.dtjg2001@gmail.com>
2026-02-07 21:23:41 -0700

    initial commit

---
+++ src/main/java/org/david/DavidGriffin_PCPartBuilderDataTypes.java
@@ -0,0 +1,82 @@
+package org.david;
+
+import java.util.Scanner;
+
+/**
+ * Author: David Griffin
+ * Date: 02/07/2026
+ * Course and Section Number: CSC110AB 11163
+ * Program Name: PC Part Builder and Compatability Checker
+ *
+ * The purpose of this program is to be an inventory management system of various different PC parts.
+ * The program will let you choose parts and add them to your builder to check for compatability. Choose the parts you want and build you dream system!
+ * Then after the builder phase you can calculate totals & check out.
+ */
+
+public class DavidGriffin_PCPartBuilderDataTypes {
+
+    public static void main(String[] args) {
+        boolean checkOutIndicator;
+        int quantityOfItem;
+        double componentPrice;
+        String computerPartCategory;
+        char modeSelection;
+
+        Scanner scnr = new Scanner(System.in);
+        // Mode Selection user prompt
+        System.out.println("**** Welcome to PC Part Picker and Compatability Checker, let's build your dream PC! ****\n");
+        System.out.print("- Select Mode: (B)uild System, (S)hop Parts, or (Q)uit: ");
+        modeSelection = scnr.next().charAt(0);
+        System.out.println("Your entered: " + modeSelection + "\n");
+
+        if (modeSelection == 'B' || modeSelection == 'b') {
+            System.out.println("Entering Build System mode... ");
+        } else if (modeSelection == 'S' || modeSelection == 's') {
+            System.out.println("Entering Shop Parts mode... ");
+        } else if (modeSelection == 'Q' || modeSelection == 'q') {
+            System.out.println("Quitting program... ");
+        }
+
+        // PC Part Category Picker user prompt
+        System.out.print("\n- Let's get building! Please enter the computer part type category you want to select parts for (CPUs, motherboards, RAM, storage (SSD/HDD), GPUs, power supplys, cases, CPU coolers): ");
+        computerPartCategory = scnr.next();
+        System.out.println("You selected " + computerPartCategory + ". Here is our inventory of " + computerPartCategory + ":");
+
+        // Quantity selction user prompt
+        System.out.print("\n- Please enter the quantity of the items: ");
+        quantityOfItem = scnr.nextInt();
+        System.out.println("You selected " + quantityOfItem + " items.");
+
+        // Enter price for inventory user prompt
+        System.out.println("\nPlease enter the price of this component: ");
+        componentPrice = scnr.nextDouble();
+        System.out.println("Your component price is: " + componentPrice + "\n");
+
+        System.out.println("Your selected item quantity: " + quantityOfItem);
+
+        // Checkout confirmation user prompt
+        System.out.println("\n- Would you like to confirm your order and checkout? Type \"True\" (Yes) or \"False\" (No): ");
+        checkOutIndicator = scnr.nextBoolean();
+
+        if (checkOutIndicator) {
+            System.out.println("Checking out now");
+        } else  {
+            System.out.println("Not checking out now\n");
+        }
+
+        System.out.println("End of Program");
+
+        // What I learned:
+        // I learned a lot in this lesson, specifically about the different data types in Java.
+        // Java is a language that is strongly typed, which means that variables have to have an associated data type with them.
+        // Some of the data types are primitives, and some of them are non-primitives, such as String.
+        // Additionally, I learned how to get user input using the Scanner class in Java and assigning that user input to a variable to then use in logic.
+        // At the heart of programming or systematic thinking is data, and essentially, what programs are is just logic applied to various different forms of data.
+        // It is essentially transformationso of data - Input > Process > Output. In this program it is - user input from Scanner > various logic > outputting user data based on various logic.
+
+        // Resources I used:
+        // https://www.baeldung.com/get-started-with-java-series, ZyBooks
+    }
+
+
+}
---
+++ src/main/java/org/david/DavidGriffin_PCPartBuilderIntro.java
@@ -0,0 +1,33 @@
+package org.david;
+
+/**
+ * Author: David Griffin
+ * Date: 01/30/2026
+ * Course and Section Number: CSC110AB 11163
+ * Program Name: PC Part Builder and Compatability Checker
+ *
+ * The purpose of this program is to be an inventory management system of various different PC parts.
+ * The program will let you choose parts and add them to your builder to check for compatability. Choose the parts you want and build you dream system!
+ * Then after the builder phase you can calculate totals & check out.
+ */
+
+public class DavidGriffin_PCPartBuilderIntro {
+    public static void main(String[] args) {
+        System.out.println("**---------------------------------------------------------------**");
+        System.out.println("Welcome to PC Part Builder and Compatibility Checker application created by David Griffin!");
+        System.out.println("**---------------------------------------------------------------**\n");
+        System.out.println("This application is a part of the computer hardware ecommerce industry.\n");
+        System.out.println("The reason why I chose this industry and project idea is that I have always wanted to build a PC, but never had a custom system for building and checking parts. ");
+        System.out.println("I want to master and develop my understanding more of hardware compatibility - what better way to do that, then to create custom logic that checks for compatability!");
+        System.out.println("And with an inventory management systems I'll be able to master various object-oriented programming concepts\n");
+        System.out.print("End of Program");
+        /*
+        What I have learned in this lesson is that Java is a high-level object-oriented programming language. Java has various syntax rules, and the code gets compiled from source code into bytecode that can run on any JVM.
+        Chapter one of Zybooks was a comprehensive overview of the foundations of computer science and introducing Java. I had previously taken a Python programming class that used Zybooks and it was very similar. I solidified some computer science concepts as well as programming basics like binary and transistors and their connection to software and decision logic.
+        I also learned about the different types of errors you can encounter as well as the structure of code in terms of thinking systematically and algorithmically.
+        I already have familiarity with Java and want to fill my gaps and improve my understanding and mastery in the language. What I did learn is that thinking systematically is important before you even write code. Making those decisions and planning the logic of your code beforehand is kind of the enabler skill that makes you a better coder and more clearly.
+        Additional resources that I have been using and loving are https://www.w3schools.com/java/ and https://www.baeldung.com/get-started-with-java-series.
+         */
+
+    }
+}
