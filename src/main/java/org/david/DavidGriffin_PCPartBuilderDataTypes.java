c9ed6c857aba2153e22b418d5f1a90af7d7700f7
DavidTJGriffin <david.dtjg2001@gmail.com>
2026-02-23 02:11:42 -0700

    updates

--- src/main/java/org/david/DavidGriffin_PCPartBuilderDataTypes.java
+++ src/main/java/org/david/DavidGriffin_PCPartBuilderDataTypes.java
@@ -48,31 +48,32 @@ public class DavidGriffin_PCPartBuilderDataTypes {
         System.out.println("You selected " + quantityOfItem + " items.");
 
         // Enter price for inventory user prompt
-        System.out.println("\nPlease enter the price of this component: ");
+        System.out.print("\nPlease enter the price of this component: ");
         componentPrice = scnr.nextDouble();
-        System.out.println("Your component price is: " + componentPrice + "\n");
-
-        System.out.println("Your selected item quantity: " + quantityOfItem);
+        System.out.println("Your component price is: $" + componentPrice);
 
         // Checkout confirmation user prompt
-        System.out.println("\n- Would you like to confirm your order and checkout? Type \"True\" (Yes) or \"False\" (No): ");
+        System.out.print("\n- Would you like to confirm your order and checkout? Type \"True\" (Yes) or \"False\" (No): ");
         checkOutIndicator = scnr.nextBoolean();
 
         if (checkOutIndicator) {
+            System.out.println("You entered " + checkOutIndicator + ".");
             System.out.println("Checking out now");
         } else  {
-            System.out.println("Not checking out now\n");
+            System.out.println("You entered " + checkOutIndicator + ".");
+            System.out.println("Not checking out now!");
         }
 
-        System.out.println("End of Program");
+        System.out.println("\nEnd of Program");
 
         // What I learned:
         // I learned a lot in this lesson, specifically about the different data types in Java.
         // Java is a language that is strongly typed, which means that variables have to have an associated data type with them.
-        // Some of the data types are primitives, and some of them are non-primitives, such as String.
-        // Additionally, I learned how to get user input using the Scanner class in Java and assigning that user input to a variable to then use in logic.
-        // At the heart of programming or systematic thinking is data, and essentially, what programs are is just logic applied to various different forms of data.
-        // It is essentially transformationso of data - Input > Process > Output. In this program it is - user input from Scanner > various logic > outputting user data based on various logic.
+        // Some of the data types are primitives and some of them are non-primitives, such as String.
+        // I also learned how to get user input using the Scanner class in Java and assigning that user input to a variable to then use in logic.
+        // At the heart of programming or systematic thinking is data and essentially what programs are is just logic applied to various different forms of data.
+        // Most programs are essentially transformations of data - Input > Process > Output.
+        // In this program it is - user input from Scanner > various logic > outputting user data based on various logic.
 
         // Resources I used:
         // https://www.baeldung.com/get-started-with-java-series, ZyBooks
